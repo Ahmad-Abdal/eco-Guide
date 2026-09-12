5cebbb9e57716e8f4ac7b564217496c9d2dc6174
ahmad-abdal <abdalahmad0313@gmail.com>
2026-09-12 15:34:12 -0700

    updated

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,4 +1,5 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 class SupabaseConfig {
-  static const String url = 'https://bdnicfklwyoqughhrebe.supabase.co';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkbmljZmtsd3lvcXVnaGhyZWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NDc5NzQsImV4cCI6MjEwNDQyMzk3NH0.GdxX1wckj3K5ujNAOrFCu1mzlW2b20EWyD9X54CuTH8';
+static const String url='suds';
+ static const String anonKey ='sd';
 }
